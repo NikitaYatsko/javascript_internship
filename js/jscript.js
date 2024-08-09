8da9638b894d708ae9cff2c8409546628fc95cea
Nikita <archon04@mail.ru>
2024-08-09 23:48:21 +0300

    task6 is donde(using else if)

--- js/jscript.js
+++ js/jscript.js
@@ -3,9 +3,11 @@ import {task2} from "./task_2.js";
 import {task3} from "./task_3.js";
 import {task4} from "./task_4.js";
 import {task5} from "./task_5.js";
+import {task6} from "./task_6.js";
 
 //task1();
 //task2();
 //task3();
 //task4();
 //task5();
+task6();
