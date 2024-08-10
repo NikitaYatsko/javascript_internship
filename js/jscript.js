93d805967e16a56cbef49d3d82efb553eb8befe9
Nikita <archon04@mail.ru>
2024-08-10 23:44:46 +0300

    iterating arrays in basic ways

--- js/jscript.js
+++ js/jscript.js
@@ -1,3 +1,11 @@
+
+import {array1} from "./arrays/array_1.js";
+array1();
+
+
+
+
+
 import {task1} from "./task_1.js";
 import {task2} from "./task_2.js";
 import {task3} from "./task_3.js";
@@ -8,7 +16,6 @@ import {task7} from "./task_7.js";
 import {task8} from "./task_8.js";
 import {task9} from "./task_9.js";
 import {task10} from "./task_10.js";
-
 //task1();
 //task2();
 //task3();
@@ -18,4 +25,4 @@ import {task10} from "./task_10.js";
 //task7();
 //task8();
 //task9();
-task10();
+//task10();
