234e4aaa0023aa93ca56987a671f0834699d8202
Nikita <archon04@mail.ru>
2024-08-10 21:38:40 +0300

    task7(using ! logic operator) is done

--- js/jscript.js
+++ js/jscript.js
@@ -4,10 +4,12 @@ import {task3} from "./task_3.js";
 import {task4} from "./task_4.js";
 import {task5} from "./task_5.js";
 import {task6} from "./task_6.js";
+import {task7} from "./task_7.js";
 
 //task1();
 //task2();
 //task3();
 //task4();
 //task5();
-task6();
+//task6();
+task7()
--- js/task_6.js
+++ js/task_6.js
@@ -10,3 +10,4 @@ export function task6() {
         alert("Error")
     }
 }
+
