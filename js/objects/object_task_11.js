3091bd8734acacd488b050e43c3786aea5169884
Nikita <archon04@mail.ru>
2024-08-31 20:54:30 +0300

    still working on task 11(found more readable way to same logic and add second function)

--- js/objects/object_task_11.js
+++ js/objects/object_task_11.js
@@ -1,33 +1,52 @@
 export function object11() {
     let option = parseInt(prompt("What option you choose?:\n" +
         "1.Show average grade for each student\n" +
-        "2."));
+        "2.Show students with average note < 5\n"));
     const arrayOfStudents = [
         {name: "Andrew", marks: [8, 10, 7, 5, 4]},
         {name: "John", marks: [4, 9, 7, 5, 9]},
-        {name: "Nick", marks: [8, 8, 8, 5, 4]},
+        {name: "Nick", marks: [2, 4, 2, 5, 4]},
         {name: "Ivan", marks: [6, 10, 7, 5, 6]},
         {name: "Saniok", marks: [8, 10, 9, 5, 8]}
     ];
 
-    switch (option) {
+    /*switch (option) {
         case 1:
             averageGrade(arrayOfStudents);
             break;
         case 2:
             lessThanFiveAverage(arrayOfStudents)
-    }
+            break;
+        case 3:
+            minMaxAverage(arrayOfStudents);
+
+    }*/
+    averageGrade(arrayOfStudents);
+    lessThanFiveAverage(arrayOfStudents);
+    minMaxAverage(arrayOfStudents);
 
     function averageGrade(array) {
-        let arrayOfAverageGrade = [];
-        array.forEach(obj => {
-            const average = obj.marks.reduce((sum, mark) => sum + mark, 0) / obj.marks.length;
-            arrayOfAverageGrade.push({name: obj.name, average: average});
-        });
-        alert(JSON.stringify(arrayOfAverageGrade, null, 1));
+        const arrayOfAverageGrade = array.map(obj => ({
+            name: obj.name,
+            average: obj.marks.reduce((sum, mark) => sum + mark, 0) / obj.marks.length
+        }))
+
+        console.log(JSON.stringify(arrayOfAverageGrade, null, 2));
     }
 
     function lessThanFiveAverage(array) {
+        const arrayLessThanFive = array.map(obj => ({
+            name: obj.name,
+            average: obj.marks.reduce((sum, mark) => sum + mark, 0) / obj.marks.length
+        }))
+            .filter(student => student.average < 5)
+
+        console.log(JSON.stringify(arrayLessThanFive, null, 2));
+    }
+
+    function minMaxAverage(array) {
+
 
     }
+
 }
