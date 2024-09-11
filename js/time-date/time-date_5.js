ced5a919b43a2cda1bbcd9f765521a2966fe2f6b
NikitaYatsko <archon04@mail.ru>
2024-09-11 19:37:36 +0300

    added small validation

--- js/time-date/time-date_5.js
+++ js/time-date/time-date_5.js
@@ -1,6 +1,9 @@
 export function timeDate5() {
     function timeConvert(minutes) {
 
+        if (minutes < 0){
+            return "Invalid data, minutes cant be < 0"
+        }
 
         let hours = Math.floor(minutes / 60);
         let rest = minutes % 60;
