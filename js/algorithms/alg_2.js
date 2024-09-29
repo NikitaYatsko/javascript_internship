d5ba9e030b060ac0375d4a9e07679ccf0dcf6674
NikitaYatsko <archon04@mail.ru>
2024-09-29 14:25:36 +0300

    hz kak reshit last zadachu po algoritmam

--- js/algorithms/alg_2.js
+++ js/algorithms/alg_2.js
@@ -1,43 +1,64 @@
-export function alg2(){
-    let data = {
+let data = {
 
-        "Рыбы": {
-            "форель": {},
-            "лосось": {}
-        },
+    "Рыбы": {
+        "форель": {}, "лосось": {}
+    },
 
-        "Деревья": {
-            "Огромные": {
-                "секвойя": {},
-                "дуб": {}
-            },
-            "Цветковые": {
-                "яблоня": {},
-                "магнолия": {}
-            }
-
+    "Деревья": {
+        "Огромные": {
+            "секвойя": {}, "дуб": {}
+        }, "Цветковые": {
+            "яблоня": {}, "магнолия": {}
         }
 
     }
 
-    function createTree(container, data) {
-        const containerElement = document.querySelector(container);
-        function createList(data) {
-            const ul = document.createElement('ul');
-            for (let key in data) {
-                const li = document.createElement('li');
-                li.textContent = key;
-                if (Object.keys(data[key]).length > 0) {
-                    li.appendChild(createList(data[key]));
-                }
-                ul.appendChild(li);
-            }
-            return ul;
-        }
-
-        containerElement.innerHTML = '';
-        containerElement.appendChild(createList(data));
-    }
-
-    createTree(".container", data);
 }
+
+/*function createTree(container, data) {
+    const containerElement = document.querySelector(container);
+
+    function createList(data) {
+        const ul = document.createElement('ul');
+        for (let key in data) {
+            const li = document.createElement('li');
+            li.textContent = key;
+            if (Object.keys(data[key]).length > 0) {
+                li.appendChild(createList(data[key]));
+            }
+            ul.appendChild(li);
+        }
+        return ul;
+    }
+
+    containerElement.innerHTML = '';
+    containerElement.appendChild(createList(data));
+}
+
+createTree(".container", data);*/
+
+
+//
+function createMatrix(row, cols) {
+    let matrix = [];
+    let value = 0;
+    for (let i = 0; i < row; i++) {
+        let row = [];
+        for (let j = 0; j < cols; j++) {
+            row.push(value++);
+        }
+
+        matrix.push(row);
+
+    }
+    return matrix;
+
+
+}
+
+console.log(createMatrix(3, 3));
+//понятия не имею как это решить.
+
+
+
+
